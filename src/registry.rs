97de4b6718efc8b9f2895d03c327b09ad97c005b
Austin Schey <aschey13@gmail.com>
2022-08-28 23:36:23 -0500

    support custom log sources

--- src/registry.rs
+++ src/registry.rs
@@ -7,53 +7,121 @@ pub mod platform {
 
     use crate::{error::RegistryError, eventmsgs};
 
-    const REG_BASEKEY: &str = r"SYSTEM\CurrentControlSet\Services\EventLog\Application";
+    const REG_BASEKEY: &str = r"SYSTEM\CurrentControlSet\Services\EventLog";
 
-    pub fn register(name: impl AsRef<str>) -> core::result::Result<(), RegistryError> {
-        let name = name.as_ref();
-        let current_exe = std::env::current_exe().map_err(RegistryError::SystemError)?;
-        let exe_path = current_exe.to_str().ok_or(RegistryError::InvalidExePath)?;
-
-        println!("exe path {exe_path}");
-        let exe_path = &exe_path.replacen("\\\\?\\", "", 1);
-        let key = Hive::LocalMachine
-            .open(REG_BASEKEY, Security::Write)
-            .map_err(map_key_error)?;
-        let app_key = key.create(name, Security::Write).map_err(map_key_error)?;
-
-        let exe_path_value = Data::String(
-            exe_path
-                .try_into()
-                .map_err(RegistryError::StrConvertError)?,
-        );
-        set_registry_value(&app_key, "EventMessageFile", &exe_path_value)?;
-
-        set_registry_value(&app_key, "CategoryMessageFile", &exe_path_value)?;
-
-        set_registry_value(
-            &app_key,
-            "CategoryCount",
-            &Data::U32(eventmsgs::CATEGORY_COUNT),
-        )?;
-
-        let supported_types =
-            EVENTLOG_ERROR_TYPE.0 | EVENTLOG_WARNING_TYPE.0 | EVENTLOG_INFORMATION_TYPE.0;
-        set_registry_value(
-            &app_key,
-            "TypesSupported",
-            &Data::U32(supported_types as u32),
-        )?;
-
-        Ok(())
+    enum SourceType {
+        Application,
+        Custom(Vec<String>),
     }
 
-    pub fn deregister(name: impl AsRef<str>) -> core::result::Result<(), RegistryError> {
-        let name = name.as_ref();
-        let key = Hive::LocalMachine
-            .open(REG_BASEKEY, Security::Read)
-            .map_err(map_key_error)?;
-        key.delete(name, true).map_err(map_key_error)?;
-        Ok(())
+    pub struct LogSource {
+        source: SourceType,
+        name: String,
+    }
+
+    impl LogSource {
+        pub fn application(name: impl Into<String>) -> Self {
+            Self {
+                source: SourceType::Application,
+                name: name.into(),
+            }
+        }
+
+        pub fn custom<'a>(
+            name: impl Into<String>,
+            sources: impl Into<Option<Vec<&'a str>>>,
+        ) -> Self {
+            let name = name.into();
+            let sources = sources.into().unwrap_or_default();
+            let mut sources: Vec<String> = sources.into_iter().map(|s| s.into()).collect();
+            sources.push(name.clone());
+            Self {
+                source: SourceType::Custom(sources),
+                name,
+            }
+        }
+
+        pub fn register(&self) -> core::result::Result<(), RegistryError> {
+            let current_exe = std::env::current_exe().map_err(RegistryError::SystemError)?;
+            let exe_path = current_exe.to_str().ok_or(RegistryError::InvalidExePath)?;
+
+            let exe_path = &exe_path.replacen("\\\\?\\", "", 1);
+
+            let key = Hive::LocalMachine
+                .open(REG_BASEKEY, Security::Write)
+                .map_err(map_key_error)?;
+
+            match &self.source {
+                SourceType::Application => {
+                    let app_key = key
+                        .open("Application", Security::Write)
+                        .map_err(map_key_error)?;
+                    let name_key = app_key
+                        .create(&self.name, Security::Write)
+                        .map_err(map_key_error)?;
+                    self.add_keys(name_key, exe_path)?;
+                }
+                SourceType::Custom(sources) => {
+                    for source in sources {
+                        let custom_key = key
+                            .create(&self.name, Security::Write)
+                            .map_err(map_key_error)?;
+                        set_registry_value(&custom_key, "AutoBackupLogFiles", &Data::U32(0))?;
+                        set_registry_value(&custom_key, "MaxSize", &Data::U32(0x00080000))?;
+                        let name_key = custom_key
+                            .create(&self.name, Security::Write)
+                            .map_err(map_key_error)?;
+                        self.add_keys(name_key, exe_path)?;
+                        let source_key = custom_key
+                            .create(source, Security::Write)
+                            .map_err(map_key_error)?;
+                        self.add_keys(source_key, exe_path)?;
+                    }
+                }
+            };
+
+            Ok(())
+        }
+
+        pub fn deregister(name: impl AsRef<str>) -> core::result::Result<(), RegistryError> {
+            let name = name.as_ref();
+            let key = Hive::LocalMachine
+                .open(REG_BASEKEY, Security::Read)
+                .map_err(map_key_error)?;
+            key.delete(name, true).map_err(map_key_error)?;
+            Ok(())
+        }
+
+        fn add_keys(
+            &self,
+            app_key: RegKey,
+            exe_path: &str,
+        ) -> core::result::Result<(), RegistryError> {
+            let exe_path_value = Data::String(
+                exe_path
+                    .try_into()
+                    .map_err(RegistryError::StrConvertError)?,
+            );
+            set_registry_value(&app_key, "EventMessageFile", &exe_path_value)?;
+
+            set_registry_value(&app_key, "CategoryMessageFile", &exe_path_value)?;
+
+            set_registry_value(
+                &app_key,
+                "CategoryCount",
+                &Data::U32(eventmsgs::CATEGORY_COUNT),
+            )?;
+
+            let supported_types =
+                EVENTLOG_ERROR_TYPE.0 | EVENTLOG_WARNING_TYPE.0 | EVENTLOG_INFORMATION_TYPE.0;
+            set_registry_value(
+                &app_key,
+                "TypesSupported",
+                &Data::U32(supported_types as u32),
+            )?;
+
+            Ok(())
+        }
     }
 
     fn set_registry_value(
--- tests/test.rs
+++ tests/test.rs
@@ -2,15 +2,15 @@
 mod tests {
 
     use tracing::info;
-    use tracing_eventlog::{register, EventLogLayer};
+    use tracing_eventlog::{EventLogLayer, LogSource};
     use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 
     #[test]
     fn super_test() {
-        let source = "tracing_eventlog_test";
-        register(source).unwrap();
+        let source = LogSource::custom("Tracing EventLog", None);
+        source.register().unwrap();
 
-        let layer = EventLogLayer::pretty(source).unwrap();
+        let layer = EventLogLayer::pretty("Tracing EventLog").unwrap();
 
         let reg = tracing_subscriber::registry().with(layer);
         let _guard = tracing::subscriber::set_default(reg);
